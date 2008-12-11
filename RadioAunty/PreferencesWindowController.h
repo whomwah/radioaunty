3da77634f3865b05cd490c15d2037773000417a0
Duncan Robertson <duncan@whomwah.com>
2008-12-11 09:41:34 +0000

    add remainder of pref pane

---
+++ RadioAunty/PreferencesWindowController.h
@@ -0,0 +1,16 @@
+//
+//  PreferencesWindowController.h
+//  RadioAunty
+//
+//  Created by Duncan Robertson on 11/12/2008.
+//  Copyright 2008 Whomwah. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PreferencesWindowController : NSWindowController {
+
+}
+
+@end
---
+++ RadioAunty/PreferencesWindowController.m
@@ -0,0 +1,28 @@
+//
+//  PreferencesWindowController.m
+//  RadioAunty
+//
+//  Created by Duncan Robertson on 11/12/2008.
+//  Copyright 2008 Whomwah. All rights reserved.
+//
+
+#import "PreferencesWindowController.h"
+
+
+@implementation PreferencesWindowController
+
+- (id)init
+{
+	if (![super initWithWindowNibName:@"Preferences"]) {
+		return nil;
+	}
+	
+	return self;
+}
+
+- (void)windowDidLoad
+{
+	NSLog(@"Nib file loaded");
+}
+
+@end
